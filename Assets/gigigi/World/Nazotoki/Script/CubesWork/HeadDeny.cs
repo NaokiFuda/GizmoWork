255f9aaa258923d06476844ffc154f4a9ecd2d6b
gizmo-pants <gigis.coutings.quad@gmail.com>
2025-04-24 07:45:10 +0900

    アバター追加したりした

---
+++ Assets/gigigi/World/Nazotoki/Script/CubesWork/HeadDeny.cs
@@ -0,0 +1,29 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Persistence;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class HeadDeny : UdonSharpBehaviour
+{
+    Collider myColloder;
+    VRCPlayerApi playerData;
+    VRCPlayerApi.TrackingData playerHead;
+    private void Update()
+    {
+        if(myColloder == null)myColloder = GetComponent<Collider>();
+        if (playerData == null)
+        {
+            playerData = Networking.LocalPlayer;
+            playerHead = playerData.GetTrackingData(VRCPlayerApi.TrackingDataType.Head);
+        }
+        if (myColloder.bounds.Contains(playerHead.position))
+        {
+            var aimPos = playerData.GetPosition();
+            var startPos = myColloder.bounds.ClosestPoint(playerHead.position);
+            aimPos.y = startPos.y;
+            playerData.SetVelocity(aimPos - startPos);
+        }
+    }
+}
