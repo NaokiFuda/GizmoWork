de924b3e7e346abec6c4c7bcdf64b6c081408eb7
gizmopants <naokifuda@gmail.com>
2025-07-03 13:21:28 +0900

    appude

---
+++ Assets/100_100/Scripts/ImmersiveCore.cs
@@ -0,0 +1,18 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ImmersiveCore : UdonSharpBehaviour
+{
+    [SerializeField] string gameTitle;
+    [SerializeField] string[] players;
+    [SerializeField] 
+    bool gameMaster;
+    bool spectator;
+    public void Initialize()
+    {
+
+    }
+}
