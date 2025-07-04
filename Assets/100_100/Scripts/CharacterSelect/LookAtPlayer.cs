6b22df6cc6b84ec567329fbbcee858a9e7cbf271
gizmopants <naokifuda@gmail.com>
2025-07-04 11:15:35 +0900

    playerNameがsyncするためにははじめからオブジェクトがある必要があったのでプレハブから外した……外したがなんかキモイのでちょっとスクリプト見直したい

---
+++ Assets/100_100/Scripts/CharacterSelect/LookAtPlayer.cs
@@ -0,0 +1,15 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class LookAtPlayer : UdonSharpBehaviour
+{
+    bool _isActive;
+    private void Update()
+    {
+        if(_isActive)
+        transform.rotation = Quaternion.identity;
+    }
+}
