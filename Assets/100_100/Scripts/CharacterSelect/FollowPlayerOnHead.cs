a97a77aff74900db3501094f7441043e860f1919
gizmo-pants <gigis.coutings.quad@gmail.com>
2025-07-05 07:22:27 +0900

    syncObjectをInstaniateするのはやめた方がよさそう。めんどくささしかないし不具合が多そう

--- Assets/100_100/Scripts/CharacterSelect/FollowPlayerOnHead.cs
+++ Assets/100_100/Scripts/CharacterSelect/FollowPlayerOnHead.cs
@@ -6,8 +6,25 @@ using VRC.Udon;
 
 public class FollowPlayerOnHead : UdonSharpBehaviour
 {
-    void Start()
+    [SerializeField] bool lookAtPlayer = true;
+    public void Activate(Transform target)
     {
-        
+        transform.parent = target;
+        transform.localPosition = Vector3.up;
+    }
+
+    void Update()
+    {
+        if (lookAtPlayer)
+        {
+            Vector3 directionToPlayer = Networking.LocalPlayer.GetPosition() - transform.position;
+            directionToPlayer.y = 0;
+
+            if (directionToPlayer != Vector3.zero)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(directionToPlayer);
+                transform.rotation = targetRotation;
+            }
+        }
     }
 }
--- Assets/100_100/Scripts/ImmersiveCore.cs
+++ Assets/100_100/Scripts/ImmersiveCore.cs
@@ -4,29 +4,38 @@ using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
+using VRC.Udon.Common.Interfaces;
 
 public class ImmersiveCore : UdonSharpBehaviour
 {
-    [SerializeField] string gameTitle;
-    [SerializeField] string[] players;
+    [SerializeField] protected string gameTitle;
+    [SerializeField] protected string[] playersName;
+    public string[] GetPlayersName {  get => playersName;  }
     GameObject[] _plSelectObj;
     [SerializeField] DisplayText textSender;
+    public DisplayText GetDisplayText { get => textSender; }
     [SerializeField] GameObject plSelectPrefab;
     TextMeshProUGUI _textPro;
-    bool _gameMaster;
-    bool _spectator;
+    protected bool _gameMaster;
+    protected bool _spectator;
+    [SerializeField] GameObject syncManager;
     private void Awake()
     {
         Initialize();
     }
     public void Initialize()
     {
-        _plSelectObj = new GameObject[players.Length];
-        for (int i =0; i< players.Length ; i++)
+        _plSelectObj = new GameObject[playersName.Length];
+        
+        for (int i =0; i< playersName.Length ; i++)
         {
-            _plSelectObj[i] = Instantiate(plSelectPrefab, transform.position + Vector3.up * 1.4f + Vector3.left* (players.Length / 2) * i, transform.rotation);
+            _plSelectObj[i] = Instantiate(plSelectPrefab, transform.position + Vector3.up * 1.4f + Vector3.left* (playersName.Length / 2) * i, transform.rotation);
+            _plSelectObj[i].GetComponent<SetPlayerButton>().immersiveCore = this;
+           
             _textPro = _plSelectObj[i].transform.GetChild(0).GetComponent<TextMeshProUGUI>();
-            _textPro.text = players[i];
+            //_textPro.text = players[i];
         }
+        syncManager.SetActive(false);
+        syncManager.SetActive(true);
     }
 }
