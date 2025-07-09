d11ed3b1e8a2fd30ea7e34f37d3f9bc64b7bca63
gizmo-pants <gigis.coutings.quad@gmail.com>
2025-07-10 01:08:24 +0900

    きをはやした

--- Assets/100_100/Scripts/CharacterSelect/PlayerStateManager.cs
+++ Assets/100_100/Scripts/CharacterSelect/PlayerStateManager.cs
@@ -9,4 +9,38 @@ public class PlayerStateManager : UdonSharpBehaviour
 {
     [UdonSynced]
     private int _playerState;
+
+    public static PlayerStateManager GetInstance(VRCPlayerApi player)
+    {
+        GameObject[] playerObjectList = Networking.GetPlayerObjects(player);
+        if (!Utilities.IsValid(playerObjectList))
+        {
+            return null;
+        }
+
+        foreach (GameObject playerObject in playerObjectList)
+        {
+            if (!Utilities.IsValid(playerObject))
+            {
+                continue;
+            }
+
+            PlayerStateManager foundScript = playerObject.GetComponentInChildren<PlayerStateManager>();
+            if (!Utilities.IsValid(foundScript))
+            {
+                continue;
+            }
+
+            return foundScript;
+        }
+
+        return null;
+    }
+
+    public void AddCount(int count)
+    {
+        _playerState += count;
+
+        RequestSerialization();
+    }
 }
