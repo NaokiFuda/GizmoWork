62183f40aac54ebd4ca014b41b18849bcdd471a5
gizmopants <naokifuda@gmail.com>
2025-07-18 10:47:22 +0900

    playroomMonsterをVRCで開発することにした

---
+++ Assets/gigigi/World/PlayRoomMonster/Script/PullCar.cs
@@ -0,0 +1,20 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class PullCar : UdonSharpBehaviour
+{
+    [SerializeField] Transform pickObj;
+    [SerializeField] Transform chainObj;
+
+    private void Start()
+    {
+        if (pickObj != null) pickObj = transform;
+    }
+    public override void Interact()
+    {
+        
+    }
+}
